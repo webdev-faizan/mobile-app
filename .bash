416f25001d8a6e33de1d7fb73bc820760c207ad0
webdev-faizan <webdev.faizanali@gmail.com>
2025-06-01 22:51:32 +0500

    a

--- .bash
+++ .bash
@@ -1,2 +1,8 @@
+#!/bin/bash
+echo "Running custom bash script"
+ls -a
+echo "Environment variables:"
+printenv
+# Add your custom commands here
 sudo arch -x86_64 gem install ffi
 sudo arch -x86_64 pod install
